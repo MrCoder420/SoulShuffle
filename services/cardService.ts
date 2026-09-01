f81f4c0ee0d83cc8caf3ef023bb02a6bac6675c5
Mrcoder420 <nikhilbhor201@gmail.com>
2026-09-01 20:41:11 +0530

    Restore missing cardService methods that caused fallback UI

---
+++ services/cardService.ts
@@ -0,0 +1,26 @@
+﻿import api from './api';
+
+export interface SendLimits {
+  daily_sent: number;
+  daily_limit: number;
+  daily_remaining: number;
+  active_count: number;
+  active_limit: number;
+  active_remaining: number;
+  can_send: boolean;
+}
+
+export const fetchCards = async () => {
+  const response = await api.get('/user/deck');
+  return response.data.data;
+};
+
+export const fetchAvailableDeck = async (roomId: string) => {
+  const response = await api.get('/user/deck/available?room_id=' + roomId);
+  return response.data.data;
+};
+
+export const fetchSendLimits = async (roomId: string): Promise<SendLimits> => {
+  const response = await api.get('/user/deck/limits?room_id=' + roomId);
+  return response.data.data;
+};
